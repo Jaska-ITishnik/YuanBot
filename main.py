9f4c535531238cb041450236428351553fee0b9f
Jaska-ITishnik <jasurbekbekmirzayev2004@gmail.com>
2025-02-02 16:08:02 +0500

    commit

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import sys
 
 import asyncpg
 import uvicorn
-from aiogram import BaseMiddleware
-from aiogram import Bot
+from aiogram import BaseMiddleware, Bot
 from aiogram import Dispatcher, F
 from aiogram.enums import ChatMemberStatus
 from aiogram.exceptions import TelegramBadRequest
@@ -49,9 +48,10 @@ ADMIN_LIST = conf.bot.ADMIN_LIST.strip().split()
 WEB_SERVER_HOST = "127.0.0.1"
 WEB_SERVER_PORT = 8088  # Changed from 80 to 8080
 WEBHOOK_PATH = "/webhook"
-# BASE_WEBHOOK_URL = "https://yuanbot.jaska-itishnik.uz"
+BASE_WEBHOOK_URL = "https://yuanbot.jaska-itishnik.uz"
 
-BASE_WEBHOOK_URL = "https://692a-178-218-201-17.ngrok-free.app"
+
+# BASE_WEBHOOK_URL = "https://cc84-178-218-201-17.ngrok-free.app"
 
 
 async def check_old_bot_membership(tg_id: int) -> bool:
